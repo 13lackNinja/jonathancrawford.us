eb2cc92f0d097a9948db0bde9fa3113bbb587a97
13lackninja <jonathan.crawford55@gmail.com>
2018-09-27 13:11:45 -0700

    simple release update

--- client/src/TopLeft.js
+++ client/src/TopLeft.js
@@ -20,23 +20,19 @@ const TopLeft = () => {
       <img id="home-logo" src={logoImage} alt="logo.svg"/>
       <table>
         <tr>
-          <a className='secondary-nav' href="https://tinyurl.com/ycd9o5qz" target="_blank" rel="noopener noreferrer">
+          <a className='secondary-nav' href="https://open.spotify.com/album/6xFuBAmV8oyyIm1KjFJhx3?si=9B6RX7ksTT6c7oWDSvqrFg" target="_blank" rel="noopener noreferrer">
             <td className='release-date'>9.27.18</td>
             <td>Simple</td>
           </a>
         </tr>
         <tr>
-          <a className='secondary-nav' href="https://fanlink.to/depths" target="_blank" rel="noopener noreferrer">
+          <a className='secondary-nav' href="https://open.spotify.com/album/1UdI7pSpywdQew7tz60E9l?si=Jr2DPsGSScaElbkR7sH53w" target="_blank" rel="noopener noreferrer">
             <td className='release-date'>5.5.17</td>
             <td>Depths</td>
           </a>
         </tr>
       </table>
-
-
     </nav>
-
-
   )
 }
 
